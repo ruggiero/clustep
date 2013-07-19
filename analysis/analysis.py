3d7249d85b213babec1ce33fd84309ef197b55d5
Rafael R <rafael.ruggiero@usp.br>
2013-07-19 20:04:00 -0300

    Organized the analysis script a little better...

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -8,6 +8,7 @@ a = 200
 Mh = 100000
 N = 100000
 G = 43007.1
+time = 0.0
 
 def hernquist_density(r):
 	if(r == 0):
@@ -77,12 +78,56 @@ def radial_speed_distribution(data, partition, aux):
 		left = right
 	return distribution
 
+def density_plot(input_, data, part, aux):
+	dist = density_distribution(data, part, aux)
+	x_axis = np.logspace(np.log10(dist[0][0]), np.log10(dist[-1][0]),\
+		num=1000)
+	p1, = plt.plot([i[0] for i in dist], [i[1] for i in dist], 'o')
+	p2, = plt.plot(x_axis, [10**10 * hernquist_density(i) for i in x_axis])
+	plt.legend([p1, p2], ["Simulation", "Theoretical value"], loc=1)
+	plt.xlabel("Radius (kpc)")
+	plt.ylabel("Density (M${_\circ}$/kpc$^3$)")
+	plt.xscale('log')
+	plt.yscale('log')
+	plt.xlim([1, 10**4])
+	plt.ylim([1, 10**10])
+	plt.title("t = %1.2f Gyr" % time)
+	plt.savefig(input_ + "-density.png")
+	plt.close()
+	print "Done with density for " + input_
+
+def radial_speed_plot(input_, data, part, aux):
+	dist = radial_speed_distribution(data, part, aux)
+	x_axis = np.logspace(np.log10(dist[0][0]), np.log10(dist[-1][0]),\
+		num=1000)
+	formatter = FuncFormatter(lambda x, pos : "%1.2f" % (x / 10**6))
+	ax = plt.subplot(111)
+	ax.yaxis.set_major_formatter(formatter)
+
+	# 0.9574 is a conversion factor from kpc^2/Gyr^2 to km^2/s^2
+	p1, = plt.plot([i[0] for i in dist], [0.9574 * i[1] for i in dist], 'o')
+	p2, = plt.plot(x_axis, 0.9574 * radial_speed_squared(x_axis))
+	plt.legend([p1, p2], ["Simulation", "Theoretical value"], loc=1)
+	plt.xlabel("Radius (kpc)")
+	plt.ylabel("$v_{r}^{2}$ (10$^{6}$ km$^{2}$/s$^{2}$)")
+	plt.xscale('log')
+	plt.title("t = %1.2f Gyr" % time)
+	#plt.gcf().subplots_adjust(left=0.17)
+	#plt.yscale('log')
+	plt.xlim([1, 10**4])
+	plt.ylim([0, 2.5 * 10**6])
+	plt.savefig(input_ + "-speed.png")
+	plt.close()
+	print "Done with speed for " + input_
+
 def main():
 	input_ = sys.argv[1]
 	snapshot = open(input_, 'r')
 	
 	# Functions from snapread.py
 	h = header(snapshot) 
+	global time
+	time = h.time
 	p_list = read_data(snapshot, h)	
 	snapshot.close()
 	data = []
@@ -92,47 +137,8 @@ def main():
 		data.append([r, vr**2])
 	data = sorted(data)
 	part, aux = log_partition(data, 2)
-
-	# Dealing with the density distribution plot
-	x = density_distribution(data, part, aux)
-	x_axis = np.logspace(np.log10(x[0][0]), np.log10(x[len(x) - 1][0]),\
-		num=1000)
-	p1, = plt.plot([i[0] for i in x], [i[1] for i in x], 'o')
-	p2, = plt.plot(x_axis, [10**10 * hernquist_density(i) for i in x_axis])
-	plt.legend([p1, p2], ["Simulation", "Theoretical value"], loc=1)
-	plt.xlabel("Radius (kpc)")
-	plt.ylabel("Density (M${_\circ}$/kpc$^3$)")
-	plt.xscale('log')
-	plt.yscale('log')
-	plt.xlim([1, 10**4])
-	plt.ylim([1, 10**10])
-	plt.title("t = %1.2f Gyr" % h.time)
-	plt.savefig(input_ + "-density.png")
-	plt.close()
-	print "Done with density for " + input_
-
-	# Now the radial speed distribution plot
-	x = radial_speed_distribution(data, part, aux)
-	x_axis = np.logspace(np.log10(x[0][0]), np.log10(x[len(x) - 1][0]),\
-		num=1000)
-	formatter = FuncFormatter(lambda x, pos : "%1.2f" % (x / 10**6))
-	ax = plt.subplot(111)
-	ax.yaxis.set_major_formatter(formatter)
-
-	# 0.9574 is a conversion factor from kpc^2/Gyr^2 to km^2/s^2
-	p1, = plt.plot([i[0] for i in x], [0.9574 * i[1] for i in x], 'o')
-	p2, = plt.plot(x_axis, 0.9574 * radial_speed_squared(x_axis))
-	plt.legend([p1, p2], ["Simulation", "Theoretical value"], loc=1)
-	plt.xlabel("Radius (kpc)")
-	plt.ylabel("$v_{r}^{2}$ (10$^{6}$ km$^{2}$/s$^{2}$)")
-	plt.xscale('log')
-	plt.title("t = %1.2f Gyr" % h.time)
-	#plt.gcf().subplots_adjust(left=0.17)
-	#plt.yscale('log')
-	plt.xlim([1, 10**4])
-	plt.ylim([0, 2.5 * 10**6])
-	plt.savefig(input_ + "-speed.png")
-	print "Done with speed for " + input_
+	density_plot(input_, data, part, aux)
+	radial_speed_plot(input_, data, part, aux)
 
 if __name__ == '__main__':
 	main()
