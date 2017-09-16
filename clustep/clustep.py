00fba0f1ea563c689ca3cf40ab11eb7d43b37b65
Rafael R <rafael.ruggiero@usp.br>
2017-09-16 16:12:07 +0200

    fix spacing

--- clustep/clustep.py
+++ clustep/clustep.py
@@ -62,27 +62,27 @@ def init():
   flags = parser(description="Generates an initial conditions file\
                 for a galaxy cluster halo simulation.")
   flags.add_argument('--gas-core', help='Sets gamma=0 in the Dehnen density\
-             profile assigned to the gas component, causing it to\
-             feature a central core. By default gamma=1, which is\
-             equivalent to a Hernquist density profile. See\
-             1993MNRAS.265..250D and 1990ApJ...356..359H.',
-             action='store_true')
+           profile assigned to the gas component, causing it to\
+           feature a central core. By default gamma=1, which is\
+           equivalent to a Hernquist density profile. See\
+           1993MNRAS.265..250D and 1990ApJ...356..359H.',
+           action='store_true')
   flags.add_argument('--dm-core', help='Exactly the same as above, but for\
-             the dark matter component.', action='store_true')
+           the dark matter component.', action='store_true')
   flags.add_argument('--no-dm', help='No dark matter particles in the\
-             initial conditions. The dark matter potential is\
-             still used when calculating the gas temperatures.',
-             action='store_true')
+           initial conditions. The dark matter potential is\
+           still used when calculating the gas temperatures.',
+           action='store_true')
   flags.add_argument('--no-gas', help='No gas, only dark matter.',
-             action='store_true')
+           action='store_true')
   flags.add_argument('-o', help='The name of the output file.',
-             metavar="init.dat", default="init.dat")
+           metavar="init.dat", default="init.dat")
   args = flags.parse_args()
   gas_core = args.gas_core
   dm_core = args.dm_core
   output = args.o
-  if not (path.isfile("header.txt") and path.isfile("params_cluster.ini")):
-    print "header.txt or params_cluster.ini missing."
+  if not (path.isfile("header.txt") and path.isfile("params_cluster.txt")):
+    print "header.txt or params_cluster.txt missing."
     exit(0)
   if args.no_dm:
     if args.no_gas:
@@ -114,7 +114,7 @@ def init():
 def inverse_cumulative(Mc, M, a, core):
   if(core):
     return ((a * (Mc**(2/3.)*M**(4/3.) + Mc*M + Mc**(4/3.)*M**(2/3.))) /
-           (Mc**(1/3.) * M**(2/3.) * (M-Mc)))
+         (Mc**(1/3.) * M**(2/3.) * (M-Mc)))
   else:
     return (a * ((Mc*M)**0.5 + Mc)) / (M-Mc)
 
@@ -152,7 +152,7 @@ def set_positions():
   if(dm):
     factor = cumulative(max_radius, M_dm, a_dm, dm_core)/M_dm
     radii_dm = inverse_cumulative(nprand.sample(N_dm) *
-                   (M_dm * factor), M_dm, a_dm, dm_core)
+                  (M_dm * factor), M_dm, a_dm, dm_core)
     thetas = np.arccos(nprand.sample(N_dm)*2 - 1)
     phis = 2 * np.pi * nprand.sample(N_dm)
     xs = radii_dm * np.sin(thetas) * np.cos(phis)
@@ -166,7 +166,7 @@ def set_positions():
   if(gas):
     factor = cumulative(max_radius, M_gas, a_gas, gas_core)/M_gas
     radii_gas = inverse_cumulative(nprand.sample(N_gas) * (M_gas * factor),
-                     M_gas, a_gas, gas_core)
+                   M_gas, a_gas, gas_core)
     thetas = np.arccos(nprand.sample(N_gas) * 2 - 1)
     phis = 2 * np.pi * nprand.sample(N_gas)
     xs = radii_gas * np.sin(thetas) * np.cos(phis)
@@ -259,7 +259,7 @@ def interpolate(E, DF_tabulated):
     dy = DF_tabulated[index][1] - DF_tabulated[index - 1][1]
     dx = DF_tabulated[index][0] - DF_tabulated[index - 1][0]
     y = (DF_tabulated[index - 1][1] + dy / dx *
-       (DF_tabulated[index - 1][0] - E))
+      (DF_tabulated[index - 1][0] - E))
     return y
 
 
@@ -324,21 +324,21 @@ def write_input_file(cluster_data):
       masses = np.concatenate((masses_gas, masses_dm))
       ids = np.arange(1, N_gas + N_dm + 1)
       write_snapshot(n_part=[N_gas, N_dm, 0, 0, 0, 0], from_text=False,
-               outfile=output,
-               data_list=[coords, vels, ids, masses, U, rho, smooths])
+             outfile=output,
+             data_list=[coords, vels, ids, masses, U, rho, smooths])
     else:
       masses = masses_gas
       ids = np.arange(1, N_gas + 1)
       write_snapshot(n_part=[N_gas, 0, 0, 0, 0, 0], from_text=False,
-               outfile=output,
-               data_list=[coords, vels, ids, masses, U, rho, smooths])
+             outfile=output,
+             data_list=[coords, vels, ids, masses, U, rho, smooths])
   else:
     ids = np.arange(1, N_dm + 1)
     masses_dm = np.empty(N_dm)
     masses_dm.fill(M_dm / N_dm)
     masses = masses_dm
     write_snapshot(n_part=[0, N_dm, 0, 0, 0, 0], from_text=False,
-             outfile=output, data_list=[coords, vels, ids, masses])
+           outfile=output, data_list=[coords, vels, ids, masses])
 
 
 if __name__ == '__main__':
